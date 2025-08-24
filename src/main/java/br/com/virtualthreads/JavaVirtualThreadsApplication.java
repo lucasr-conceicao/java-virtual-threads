2b80975814031bf7ea4f6e5a42ec1ac6ed39dde2
lucasr-conceicao <lucasr-conceicao@outlook.com.br>
2025-08-23 23:51:30 -0300

    chore: Configuracao inicial aplicacao virtual threads

---
+++ src/main/java/br/com/virtualthreads/JavaVirtualThreadsApplication.java
@@ -0,0 +1,13 @@
+package br.com.virtualthreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaVirtualThreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaVirtualThreadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/virtualthreads/JavaVirtualThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.virtualthreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVirtualThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
